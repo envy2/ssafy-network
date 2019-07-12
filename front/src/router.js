aff2e58f4024a1ae14f8ce164e4bc328ca189f49
mwh5026 <auddn9876@naver.com>
2019-07-12 15:23:32 +0900

    folder tree setting

--- front/src/router.js
+++ front/src/router.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import HomePage from './views/HomePage.vue'
+import NotePage from './views/NotePage.vue'
+import AboutPage from './views/AboutPage.vue'
+import CodePage from './views/CodePage.vue'
+import SocialPage from './views/SocialPage.vue'
+
 
 Vue.use(Router)
 
@@ -9,15 +14,25 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HomePage
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: AboutPage
+    },{
+      path: '/code',
+      name: 'code',
+      component: CodePage
+    },{
+      path: '/social',
+      name: 'social',
+      component: SocialPage
+    },{
+      path: '/note',
+      name: 'note',
+      component: NotePage
     }
+
   ]
 })
---
+++ front/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <div class="about">
+    <h1>This is an Home page</h1>
+      <!-- session 값 있을때 바로 페이지 -->
+    <div v-if="true">
+      
+    </div>
+    <!-- session 값 없을때 로그인 창 -->
+    <div v-if="false">
+
+    </div>
+  </div>
+</template>
